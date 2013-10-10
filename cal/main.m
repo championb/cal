9b40e485ec1adfbee0e2f7e6bca402c60bdbb3ba
championb <championb@championbtekiMac.local>
2013-10-10 02:25:16 -0700

    Initial Commit

---
+++ cal/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cal
+//
+//  Created by championb on 13-10-10.
+//  Copyright (c) 2013年 championb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
